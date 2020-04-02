1e4d89aeecef2196104c61dbefa6ab955196ca8e
Ethan Lee <ethanlee515@gmail.com>
2020-04-02 15:00:49 +0000

    Fix bug

--- proxy.ts
+++ proxy.ts
@@ -8,11 +8,11 @@ const server = net.createServer(socket => {
 	if(client1) {
 		client2 = socket;
 		other = client1;
-		console.log('first client connected.');
+		console.log('second client connected.');
 	} else {
 		client1 = socket;
 		other = client2;
-		console.log('second client connected.');
+		console.log('first client connected.');
 	}
 	socket.on('data', data => other.write(data));
 });
