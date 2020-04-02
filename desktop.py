90be90a008295ef4fe04e111ec53c54984fd3fa6
Ethan Lee <ethanlee515@gmail.com>
2020-04-03 00:23:08 +0800

    Fix bugs

--- desktop.py
+++ desktop.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3.7
 
 import socket
+import select
 import asyncio
 
-aws = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-aws.connect(('random-rpg.com', 8030))
+aws_persistent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+aws_persistent.connect(('random-rpg.com', 8030))
+
+pairs = list()
 
 while True:
-	_ = aws.recv(1)
-	asyncio.create_subprocess_exec("./desktop2.py")
+	to_select = [item for pair in pairs for item in pair]
+	to_select.append(aws)
+	readable, _, _ = select.select(to_select, [], [])
+	if aws_persistent in readable:
+		_ = aws_persistent.recv(1)
+		aws = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		aws.connect(('random-rpg.com', 8032))
+
+		self_ssh = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self_ssh.connect(('localhost', 22))
+		pairs.append((aws, self_ssh))
+	
+	for pair in pairs:
+		if pair[0] in readable:
+			pair[1].send(pair[0].recv(1024))
+		if pair[1] in readable:
+			pair[0].send(pair[1].recv(1024))
